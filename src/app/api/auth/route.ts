e045891bca2749fbd8525e64b8d296057023341e
murodjon <murodjon.muzaffarov1@gmail.com>
2025-01-06 20:38:09 +0500

    chore

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -6,9 +6,11 @@ const privateKey = process.env.PRIVATE_KEY;
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token = searchParams.get("token") || crypto.randomUUID();
+
   const expire =
     searchParams.get("expire") ||
     (Math.floor(Date.now() / 1000) + 2400).toString();
+
   const privateAPIKey = privateKey;
   const signature = crypto
     .createHmac("sha1", privateAPIKey)
